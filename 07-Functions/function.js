ed2acee8befc957fdc2e63f9d06f33a7f63e3a3d
atif rafique <atif@gmail.com>
2026-06-02 23:47:51 +0500

    basic function

--- 07-Functions/function.js
+++ 07-Functions/function.js
@@ -1,1 +1,14 @@
 // functions in js
+
+// Basic function
+function hello (say){
+    console.log(say)
+}
+// hello('hello atif')
+
+// function exparsion
+let car = function (action){
+    console.log(`car is ${action}`)
+}
+car('running')
+car('stop')
