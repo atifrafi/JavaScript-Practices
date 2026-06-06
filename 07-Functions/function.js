2bb5e2ff9c74bbbb51ac4f7f58d1fca0b0abfbd3
atif rafique <atif@gmail.com>
2026-06-06 20:54:20 +0500

    basic functions

--- 07-Functions/function.js
+++ 07-Functions/function.js
@@ -12,3 +12,34 @@ let car = function (action){
 }
 car('running')
 car('stop')
+
+// function exparsion with return
+let addFunc = function (x,y){
+    return x * y
+}
+console.log(addFunc(20,5))
+
+
+// higher order or callback function kya hai ?
+// callback function 
+// 1. Callback function kya hota hai?
+// Callback function woh function hota hai jo kisi dusre function ko argument ke taur par pass kiya jata hai,
+//  aur baad mein execute hota hai.
+
+// 2. Higher-Order Function kya hota hai?
+// Higher-order function woh function hota hai jo:
+// - ya to ek function ko argument leta hai
+// - ya ek function return karta hai
+
+
+// normal function 
+let higherOrderFunc = function (a){
+    return a()
+}
+
+let callbackFunc = function (){
+    console.log('hello from callback')
+}
+
+// console.log(higherOrderFunc(callbackFunc)) // ya undefined dega console ki waja sa
+higherOrderFunc(callbackFunc)
