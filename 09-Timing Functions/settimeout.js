2316dc954ab30f771ac87bafb7392467f871511d
atif rafique <atif@gmail.com>
2026-06-07 11:31:01 +0500

    settimeout

---
+++ 09-Timing Functions/settimeout.js
@@ -0,0 +1,9 @@
+// setTimeout function
+// ya particular time ka baad run hota ha ek dafa
+
+let timeout = setTimeout(()=>{
+    // for(let i=1; i<5; i++){
+    //     console.log(i)
+    // }
+    console.log('hello')
+},10000)
