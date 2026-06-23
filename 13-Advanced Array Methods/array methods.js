ba9c45ce22a2a153ac1871a0bc64d9be675e6ef3
atif rafique <apt848696@gmail.com>
2026-06-23 17:56:36 +0500

    map practice

--- 13-Advanced Array Methods/array methods.js
+++ 13-Advanced Array Methods/array methods.js
@@ -1,10 +1,22 @@
 // array methods
 
 // map 
-// filter 
-// reduce
+// important point map technicaly loop chalata hai. lakin 
+// iska main maqsad array ka loop chalana nhi hota.
+// ya array ko transform karka naya array banana hota hai.
+let arr=[2,4,6,8,10]
 
-// ajsdhkasdad
-// asdas
+let transformArray = arr.map((x)=>{
+    return x * 2
+})
 
-// nothing 
+console.log(transformArray)
+
+// map ka naye array pr loop 
+// for each method
+let looping = transformArray.forEach((num)=>{
+    console.log(num)
+})
+
+
+
