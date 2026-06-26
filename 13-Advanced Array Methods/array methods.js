480fc0c465731376e2c43f2448e22e5dda839fce
atif rafique <apt848696@gmail.com>
2026-06-27 01:29:13 +0500

    filter and reduce method cover

--- 13-Advanced Array Methods/array methods.js
+++ 13-Advanced Array Methods/array methods.js
@@ -21,8 +21,39 @@ let looping = transformArray.forEach((num)=>{
 
 
 // filter 
+// ya new array return karta hai 
+let carBrands = ['Mercedes','volks wagon','lamborgheni', 'ferrari', 'porche', 'koenigsegg','BMW','toyota','hundayi','baleno']
+// filter by alphbets
+let filterByAlpha = carBrands.filter((brandnames)=>{
+    return brandnames.toLowerCase().startsWith('b');
+})
+console.log(filterByAlpha)
 
-// kdskd
-// mmm
-// 
-// for each 
+
+
+// reduce 
+// reduce bhi naya array return karta hai
+// reduce ka matlab kam karna nhi ya pora array ko compress karka 
+// single value deta hai
+let arr2=[1,2,3,4]
+let result = arr2.reduce((acc,curr)=>{
+    return acc + curr
+},0)
+
+console.log(result ,'result')
+
+// working 
+/*
+acc = 0  + curr = 1 total is 1
+acc = 1  + curr = 2 total is 3
+acc = 3  + curr = 3 total is 6
+acc = 6  + curr = 4 total is 10
+*/
+
+// other example with string
+let arr3 = ['h','i','e','g']
+let resultarr3 = arr3.reduce((acc,curr)=>{
+    return acc + curr
+},"")
+
+console.log(resultarr3 ,'result')
